da99d4c65bcef5570c99bdb45a488db82166efba
pradiep kummaar <pky463775@gmail.com>
2023-12-27 11:21:54 +0530

    inital commit

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host="localhost";
+$username="root";
+$password="";
+$database="php_db";
+
+$conn=new mysqli($host,$username,$password,$database);
+
+if ($conn->connect_error) {
+	die(`connection failed`.$conn->connect_error);
+} 
+echo "Connected Successfully";
+$conn->close();
+
+?>
